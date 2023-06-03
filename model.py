5d9d9b0ee0fc70500208e9b87baedd48075a3dcc
Andrey <remorov.andrey@gmail.com>
2023-06-03 13:56:11 -0400

    changed mu to size([B,1]) from [B,2]

--- model.py
+++ model.py
@@ -40,7 +40,8 @@ class RecurrentAttention(nn.Module):
 
         self.sensor = modules.GlimpseNetwork(h_g, h_l, g, k, s, c)
         self.rnn = modules.CoreNetwork(hidden_size, hidden_size)
-        self.locator = modules.LocationNetwork(hidden_size, 2, std)
+        # changed to 1 since only single value needed
+        self.locator = modules.LocationNetwork(hidden_size, 1, std)
         self.classifier = modules.ActionNetwork(hidden_size, num_classes)
         self.baseliner = modules.BaselineNetwork(hidden_size, 1)
 
